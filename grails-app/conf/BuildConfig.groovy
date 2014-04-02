1d322024683c3759a1bcafab46ddb1e02db8d350
David Estes <davydotcom@gmail.com>
2014-04-02 09:32:33 -0400

    adjusting version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,7 +14,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":asset-pipeline:1.7.5"
+        runtime ":asset-pipeline:1.7.6"
 
         build(":release:2.2.1",
               ":rest-client-builder:1.0.3") {
