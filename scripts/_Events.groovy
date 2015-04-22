876199b764cf4c278bf878c58d3bd97f6099c591
Gabe Hamilton <gabehamilton@gmail.com>
2015-04-22 13:45:13 -0600

    Removing jars & gems from production war

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -9,3 +9,12 @@ eventAssetPrecompileStart = { assetConfig ->
     }
     configHolder.config.sass.resolveGems = false
 }
+// Remove the JRuby jar and gems directory before the war is bundled
+eventCreateWarStart = { warName, stagingDir ->
+	if (grailsEnv == "production") {
+		grailsConsole.log "Removing JRuby jar and gems used for Sass compilation in Dev"
+		Ant.delete(file:"${stagingDir}/WEB-INF/lib/jruby-complete-1.7.11.jar")
+		Ant.delete(file:"${stagingDir}/WEB-INF/lib/jruby-container-0.4.0.jar")
+		Ant.delete(dir:"${stagingDir}/WEB-INF/classes/gems")
+	}
+}
