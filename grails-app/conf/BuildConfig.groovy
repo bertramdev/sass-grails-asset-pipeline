dedf334b97693f4922cd7284df8a457e58dbb97b
David Estes <davydotcom@gmail.com>
2016-03-09 09:52:44 -0500

    version bumping

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,11 +12,11 @@ grails.project.dependency.resolution = {
         jcenter()
     }
     dependencies {
-        compile 'com.bertramlabs.plugins:sass-asset-pipeline:2.7.0'
+        compile 'com.bertramlabs.plugins:sass-asset-pipeline:2.7.2'
     }
 
     plugins {
-        runtime ":asset-pipeline:2.7.0"
+        runtime ":asset-pipeline:2.7.2"
 
         build(":release:3.1.2",
               ":rest-client-builder:2.0.3") {
