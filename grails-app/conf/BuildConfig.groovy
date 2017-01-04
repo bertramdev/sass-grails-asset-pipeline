bc0e6150a04caedf0bace18770d63279bf4c82f6
David Estes <davydotcom@gmail.com>
2017-01-04 09:49:59 -0500

    fixing annoyng log

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,11 +12,11 @@ grails.project.dependency.resolution = {
         jcenter()
     }
     dependencies {
-        compile 'com.bertramlabs.plugins:sass-asset-pipeline:2.12.3'
+        compile 'com.bertramlabs.plugins:sass-asset-pipeline:2.12.4'
     }
 
     plugins {
-        runtime ":asset-pipeline:2.12.3"
+        runtime ":asset-pipeline:2.12.4"
 
         build(":release:3.1.2",
               ":rest-client-builder:2.0.3") {
